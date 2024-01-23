b23f8a1c4af7aa0c43cef4e8eaf7204a52d46210
Dewesh Jha <deweshjha4@gmail.com>
2024-01-24 00:51:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const generateBtn = document.getElementById('generateBtn');
+
+generateBtn.addEventListener('click',generateTestCase);
+
+async function generateTestCase(){
+    const datastructure = document.getElementById('datastructure').value;
+    if(!datastructure){
+        alert('Invalid Option');
+        return;
+    }
+    
+    const url = `https://lc-tcg-api.cyclic.app/api/${datastructure}/0`;
+
+    const headersList = {
+        "Accept": "*/*"
+    }
+
+    let response = await fetch(url, {
+        method: "GET",
+        headers: headersList
+    });
+
+    let data = await response.json();
+    // console.log(data);
+    document.getElementById('generatedData').innerHTML = data[datastructure];
+}
